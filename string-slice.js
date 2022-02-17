71bed8d2be4b58bc26657fcdd797ca4a3e411ac0
noman-swe <noman.swe130@gmail.com>
2022-02-17 14:59:14 +0600

    29-3:substring(start, end), [-updated:- split(), Slice(start, end), substr(start, no of letter amount) -]

--- string-slice.js
+++ string-slice.js
@@ -1,12 +1,17 @@
+// split()
 const anthem = 'Amar Shonar Bangla, Ami Tomai Valobashi';
 const words = anthem.split(' ');
 const withOutA = anthem.split('a');
-//console.log(words);
+console.log(words);
 
-//Slice() split
+//Slice(start, end) 
 const smallSlice = anthem.slice(5, 13);
-//console.log(smallSlice);
+console.log(smallSlice);
 
-// substr
+// substr(start, no of letter amount)
 const anotherPart = anthem.substr(10, 1);
 console.log(anotherPart);
+
+// substring(start, end)
+const anotherAnotherPart =  anthem.substring(5, 13);
+console.log(anotherAnotherPart);
