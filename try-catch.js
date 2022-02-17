577633327cdb3ece5da706627cae5944cc13175e
noman-swe <noman.swe130@gmail.com>
2022-02-17 17:22:41 +0600

    29-8 Different types of Error object and explore Try-catch

---
+++ try-catch.js
@@ -0,0 +1,15 @@
+const myName = 'Ami';
+
+/* for(let i = 0; i < 10; i++){
+    console.log(i);
+} */
+
+
+try{
+    myName = 'Tumi';
+}catch(error){
+    console.log(error);
+}
+//finally{} ->need to see
+// throw -> need to see
+console.log(myName);
